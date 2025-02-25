ab8c23ed13fd4919333b665c0d1c5a50c7b98f13
Seelma <selmaelmaaroufi9@gmail.com>
2025-02-25 02:16:37 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,9 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Show columns of the books table
-SHOW COLUMNS FROM books;
-mysql -u root -p your_database_name < task_4.sql
+-- Select column information for the books table from INFORMATION_SCHEMA
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
+mysql -u root -p < task_4.sql
